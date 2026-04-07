8f470d270bd9a9d4c385256d16cde33e44396c34
Abah Godswill <godswillabah1995@gmail.com>
2026-04-07 08:58:05 +0100

    added payment failed template

--- payment/views.py
+++ payment/views.py
@@ -255,3 +255,7 @@ def checkout(request):
 def payment_success(request):
     
     return render(request,'payment/payment_success.html',{}) 
+
+def payment_failed(request):
+    
+    return render(request,'payment/payment_failed.html',{}) 
