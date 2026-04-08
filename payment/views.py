e4db26ae21aa9712c89b169befa9090dfe26da32
Abah Godswill <godswillabah1995@gmail.com>
2026-04-08 21:46:46 +0100

    fixed data

--- payment/views.py
+++ payment/views.py
@@ -235,8 +235,8 @@ def process_order_verify(request):
                 product_id = str(item.id)
                 OrderItems.objects.create(order=new_order,products=item,user=request.user if request.user.is_authenticated else None,quantity=quantities[product_id],price=item.price if not item.is_sale else item.sale_price)
             
-            #request.session.pop('cart',None)
-            #request.session.modified = True
+            request.session.pop('cart',None)
+            request.session.modified = True
 
             return render(request,'payment/payment_success.html',{'order':new_order})
     else:
